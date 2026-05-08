5ee03291e2453153df19be5675aa5304e4a9eff9
vazquezharo <vazquezh16@gmail.com>
2026-05-08 11:03:27 -0400

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ import {
   UtensilsCrossed,
   type LucideIcon,
 } from "lucide-react";
-import { siteConfig, type ScheduleIconName } from "@/config/site";
+import { siteConfig, type ScheduleIconName } from "../config/site";
 
 export const dynamic = "force-static";
 
