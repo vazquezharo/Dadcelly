b99b949370e51692e94c0f310e1ffdaeb83e8fe3
vazquezharo <vazquezh16@gmail.com>
2026-05-08 07:43:44 -0400

    Fix import path for siteConfig in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { siteConfig } from "@/config/site";
+import { siteConfig } from "../config/site";
 import "./globals.css";
 
 export const metadata: Metadata = {
